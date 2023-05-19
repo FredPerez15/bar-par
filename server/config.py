ab84698c50d6cac3da5a53df748e17677986aae4
Freddy Perez <fred.perez15@gmail.com>
2023-05-19 12:47:09 -0400

    Login and backend setup halfway

--- server/config.py
+++ server/config.py
@@ -1,29 +1,25 @@
-from flask_restful import Api
-from flask_bcrypt import Bcrypt
-from flask_migrate import Migrate
-from flask import Flask, abort
-from flask_sqlalchemy import SQLAlchemy
-import os
-
-from dotenv import load_dotenv
-load_dotenv()
+# from flask_restful import Api
+# from flask_migrate import Migrate
+# from flask import Flask, abort
+# from flask_sqlalchemy import SQLAlchemy
+# import os
 
 
-app = Flask(
-    __name__,
-    static_url_path='',
-    static_folder='../client/build',
-    template_folder='../client/build'
-)
 
-app.secret_key = b'R\x83\ru\xf0\xe2\x92A\xad\x922Z\x19y\xc4n'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+# app = Flask(
+#     __name__,
+#     static_url_path='',
+#     static_folder='../client/build',
+#     template_folder='../client/build'
+# )
 
-db = SQLAlchemy()
 
-bcrypt = Bcrypt(app)
-migrate = Migrate(app, db)
-db.init_app(app)
-api = Api(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.json.compact = False
+
+
+# # db = SQLAlchemy()
+# migrate = Migrate(app, db)
+# # db.init_app(app)
+# # api = Api(app)
--- server/models.py
+++ server/models.py
@@ -2,11 +2,14 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
-from sqlalchemy.ext.hybrid import hybrid_property
-from config import db, bcrypt
+
+
+db = SQLAlchemy()
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
+
+    serialize_rules = ('-created_at', '-updated_at')
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True)
@@ -15,23 +18,51 @@ class User(db.Model, SerializerMixin):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    recipes = db.relaltionsip('Recipe', backref='user' cascade='all, delete, delete-orphan')
+    recipes = db.relationship('Recipe', backref='user', cascade='all, delete, delete-orphan')
+
+    @validates('username')
+    def valid_username(self, key, username):
+        if not username:
+            raise ValueError('Username is required')
+        return username
+
+    @validates('email')
+    def valid_email(self, key, email):
+        if not email:
+            raise ValueError('Email is required')
+        return email
+
+    @validates('password')
+    def valid_pword(self, key, pword):
+        if not pword:
+            raise ValueError('Password is required')
+        return pword
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
 
+    serialize_rules = ('-created_at', '-updated_at', '-inventories', 'ingredients')
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     description = db.Column(db.String)
-    user_id = db.Column(db.Integer. db.ForeignKey('users.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    inventories = db.relaltionsip('Inventory', backref='recipe', cascade='all, delete, delete-orphan')
+    inventories = db.relationship('Inventory', backref='recipe', cascade='all, delete, delete-orphan')
     ingredients = association_proxy('inventories', 'recipe')
+
+    @validates('name')
+    def valid_username(self, key, name):
+        if not name:
+            raise ValueError('Name is required')
+        return name
 
 class Ingridient(db.Model, SerializerMixin):
     __tablename__ = 'ingredients'
+
+    serialize_rules = ('-created_at', '-updated_at', '-recipe_id')
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -41,15 +72,33 @@ class Ingridient(db.Model, SerializerMixin):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('name')
+    def valid_username(self, key, name):
+        if not name:
+            raise ValueError('Name is required')
+        return name
+
+    @validates('ing_type')
+    def valid_username(self, key, ing_type):
+        if not ing_type:
+            raise ValueError('Type of Ingridient is required')
+        return ing_type
+
 class Inventory(db.Model, SerializerMixin):
     __tablename__ = 'inventories'
 
+    serialize_rules = ('-created_at', '-updated_at', '-recipe_id', '-recipes', '-ingredients')
+
     id = db.Column(db.Integer, primary_key=True)
     quantity = db.Column(db.Integer)
-    ingrdient_id = db.Column(db.Integer, db.ForeignKey('ingredients.id'))
+    ingridient_id = db.Column(db.Integer, db.ForeignKey('ingredients.id'))
     recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id')) 
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-    
-    recipes
 
+    @validates('quantity')
+    def valid_username(self, key, quantity):
+        if not quantity:
+            raise ValueError('Quantity is a required field')
+        return quantity
+    
